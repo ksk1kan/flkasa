a8b65ff97a3668e9e95c06dde24a14cd4c048268
Birkan A.F. <59991587+ksk1kan@users.noreply.github.com>
2025-08-12 12:48:02 +0300

    updated all folder
    v6.4

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,30 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('kasa-cache').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './favicon.png'
-      ]);
-    })
-  );
+const CACHE_NAME = 'kasa-takip-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request).then(fRes => {
+      const copy = fRes.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+      return fRes;
+    }).catch(() => caches.match('./index.html')))
   );
 });
